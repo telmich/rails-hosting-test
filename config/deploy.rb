306f86c4df0418a664269ddb3894307061eaa6fa
Nico Schottelius <nico@brief.schottelius.org>
2013-03-24 14:47:58 +0100

    capify .
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,25 @@
-require 'capistrano-rbenv'
-require 'bundler/capistrano'
+set :application, "set your application name here"
+set :repository,  "set your repository location here"
 
-set :rbenv_ruby_version, "2.0.0" 
-set :rbenv_install_dependencie, false"
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :rails_env, 'production'
-set :application 'rails-hosting-test'
+role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
 
-# Deployment source
-set :scm, :git
-set :repository, "git@github.com:telmich/rails-hosting-test.git"
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
 
-# Deployment method
-set :deploy_via, :remote_cache
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
 
-# Deployment destination
-server 'wittbib-staging.panter.ch', :app, :web, :db, :primary => true
-set :deploy_to, "/home/rails/app"
-set :user, "rails"
-
-# Forward agent so cloning from github is possible
-set :ssh_options, {:forward_agent => true}
-
-# Don't try to use sudo - all deps should already be there!
-set :use_sudo, false
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
