a2f4151ad3646c69e4afc86dfed65a4618c7936e
Nico Schottelius <nico@brief.schottelius.org>
2013-03-24 14:21:13 +0100

    init
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsHostingTest::Application.config.secret_token = '47252e51a16ee6e3d3723e71c673e825a4d4b4475fc981efa1a81585f2d7d6fcdf39805a59535ea2352f5ad0045d5e8951ff6c34a04fc0bf1c8593e896b66a84'
