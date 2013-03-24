2be1e8e8692ffc272da13e7a541df38549460968
Nico Schottelius <nico@brief.schottelius.org>
2013-03-24 15:40:56 +0100

    integrate unicorn into capistrano workflow
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,3 +20,29 @@ set :ssh_options, {:forward_agent => true}
 
 # Don't try to use sudo - all deps should already be there!
 set :use_sudo, false
+
+# Unicorn integration
+set :unicorn_binary, "bundle exec unicorn --listen #{current_path}/unicorn.sock --env #{rails_env} --daemonize'"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+ 
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && #{unicorn_binary}"
+  end
+ 
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then kill `cat #{unicorn_pid}`; fi"
+  end
+ 
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then kill -s QUIT `cat #{unicorn_pid}`; fi"
+  end
+ 
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "if [ -f #{unicorn_pid} ]; then kill -s USR2 `cat #{unicorn_pid}`; fi"
+  end
+ 
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
